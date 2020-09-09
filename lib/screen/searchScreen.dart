2b4c9c34ed007c8a2a0373b3f231d5b02d6799f1
Vanshika Agrawal <vanshika.agrawal_cs18@gla.ac.in>
2020-09-09 11:45:20 +0530

    added padding in searchCard.

--- lib/screen/searchScreen.dart
+++ lib/screen/searchScreen.dart
@@ -94,30 +94,44 @@ class _SearchScreenState extends State<SearchScreen> {
                             itemBuilder: (context, index) {
                               DocumentSnapshot data = snapshot.data.docs[index];
                               return Card(
-                                child: Row(
-                                  children: <Widget>[
-                                    // Image.network(
-                                    //   data['imageUrl'],
-                                    //   width: 150,
-                                    //   height: 100,
-                                    //   fit: BoxFit.fill,
-                                    // ),
-                                    SizedBox(
-                                      width: 25,
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        print('taped');
-                                      },
-                                      child: Text(
-                                        data.data()['username'],
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 20,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      // Image.network(
+                                      //   data['imageUrl'],
+                                      //   width: 150,
+                                      //   height: 100,
+                                      //   fit: BoxFit.fill,
+                                      // ),
+                                      SizedBox(
+                                        width: 25,
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          print('taped');
+                                        },
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              data.data()['username'],
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 20,
+                                                color: Colors.indigo,
+                                              ),
+                                            ),
+                                            Text('Faculty',
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               );
                             },

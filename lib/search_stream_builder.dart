06eeca6c1f2e5de8e71b4fd79336455da793e89b
ritik <ritik.srivastava_cs18@gmail.com>
2020-09-11 22:45:35 +0530

    added message sending and receiving features in the chat screen.

--- lib/search_stream_builder.dart
+++ lib/search_stream_builder.dart
@@ -13,9 +13,9 @@ class SearchStreamBuilder extends StatelessWidget {
     return StreamBuilder<QuerySnapshot>(
       stream: (name != "" && name != null)
           ? FirebaseFirestore.instance
-          .collection('users')
-          .where("searchKeywords", arrayContains: name)
-          .snapshots() //TODO: Recent searches to be added here
+              .collection('users')
+              .where("searchKeywords", arrayContains: name)
+              .snapshots() //TODO: Recent searches to be added here
           : FirebaseFirestore.instance.collection("users").snapshots(),
       builder: (context, snapshot) {
         return (snapshot.connectionState == ConnectionState.waiting)
@@ -26,16 +26,16 @@ class SearchStreamBuilder extends StatelessWidget {
             DocumentSnapshot data = snapshot.data.docs[index];
             return Card(
               child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8.0,
-                        horizontal: 16.0,
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          (data.data()['profileURL'] != null)
-                              ? CircleAvatar(
-                                  radius: 25.0,
-                                  child: ClipOval(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 16.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    (data.data()['profileURL'] != null)
+                        ? CircleAvatar(
+                      radius: 25.0,
+                      child: ClipOval(
                                     child: SizedBox(
                                       width: 50.0,
                                       height: 50.0,
@@ -66,14 +66,14 @@ class SearchStreamBuilder extends StatelessWidget {
                           SizedBox(
                             width: 25.0,
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              print('taped');
-                            },
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
+                    GestureDetector(
+                      onTap: () {
+                        print('taped');
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
                             StringUtils.capitalize(
                                 data.data()['username']),
                             style: KSearchDisplayNameTextStyle,

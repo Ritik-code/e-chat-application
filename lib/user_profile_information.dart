c46bba55dc45243045726f75a49539355de2daba
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-09-17 09:51:12 +0530

    add method to validate password

---
+++ lib/user_profile_information.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class UserProfileInformation{
+
+ Future<bool> validatePassword(String password) async {
+    var firebaseUser = await _auth.currentUser();
+
+    var authCredentials = EmailAuthProvider.getCredential(
+        email: firebaseUser.email, password: password);
+    try {
+      var authResult = await firebaseUser
+          .reauthenticateWithCredential(authCredentials);
+      return authResult.user != null;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+}

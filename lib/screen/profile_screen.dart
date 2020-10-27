ef224d2611427ad0d5a0ad2d22449a3e249ddffb
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-10-28 00:17:20 +0530

    add edit profile picture functionality

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,157 +1,198 @@
-// To-Do:
-// -when viewing own profile:
-// fetch current user name, email and profile photo, provide edit options,  don't show send message button
-// -when viewing other person profile:
-// fetch searched result's name, email and profile photo, show send message button
-
-// -when viewing faculty profile:
-// show rating
-// -when viewing student profile:
-// don't show rating
-
-// - adding rating mechanism
-
-// - show @professor when faculty and @student when student's profile
-
-import 'package:basic_utils/basic_utils.dart';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:comperio/helper_functions.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:path/path.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String id = 'ProfileScreen';
-
-  // File _image;
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  QuerySnapshot snapshotUserInfo;
   String userName;
   String emailId;
   String picURL;
+  File _image;
+  bool showSpinner=false;
+  String url;
 
-  getUserInfo() async {
-    userName = await HelperFunctions.getUserNameSharedPreference();
-    emailId = await HelperFunctions.getUserEmailSharedPreference();
-    picURL = await HelperFunctions.getUserPhotoUrlSharedPreference();
+
+  getUserInfo() async{
+      String username = await HelperFunctions.getUserNameSharedPreference();
+      String email = await HelperFunctions.getUserEmailSharedPreference();
+      var Url = await Firestore.instance.collection('users').document(username).get();
+      setState(() {
+        userName = username;
+        emailId = email;
+        picURL = Url.data()['profileURL'];
+      });
   }
 
   @override
   void initState() {
-    getUserInfo();
     super.initState();
+      getUserInfo();
+
   }
 
-  //  Future getImages() async {
-  //   PickedFile pickedFile =
-  //       await ImagePicker().getImage(source: ImageSource.gallery);
-  //   var image = File(pickedFile.path);
+   Future getImages() async {
+    PickedFile pickedFile =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    var image = File(pickedFile.path);
 
-  //   setState(() {
-  //     _image = image;
-  //     // print('Image path $_image');
-  //   });
-  // }
+    setState(() {
+      _image = image;
+      // print('Image path $_image');
+    });
+  }
 
-  //  Future uploadPic(BuildContext context) async {
-  //   String fileName = basename(_image.path);
-  //   StorageReference firebaseStorageRef =
-  //       FirebaseStorage.instance.ref().child(fileName);
-  //   StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
-  //   var dowUrl = await (await uploadTask.onComplete).ref.getDownloadURL();
-  //   url = dowUrl.toString();
-  //   print(url);
-  //   StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
-  //   setState(() {
-  //     print("Profile Picture uploaded");
-  //     // Scaffold.of(context)
-  //     //     .showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
-  //   });
-  // }
+   Future uploadPic(BuildContext context) async {
+    String fileName = basename(_image.path);
+    StorageReference firebaseStorageRef =
+        FirebaseStorage.instance.ref().child(fileName);
+    StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+    var dowUrl = await (await uploadTask.onComplete).ref.getDownloadURL();
+
+      url = dowUrl.toString();
+
+    // print(url);
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    setState(() {
+      print("Profile Picture uploaded");
+      // Scaffold.of(context)
+      //     .showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            constraints: BoxConstraints.expand(),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        "https://i.pinimg.com/564x/84/c0/19/84c019fbb489f4025ba4f2b8754582c3.jpg"),
-                    fit: BoxFit.cover)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 7.0,
-                        color: Colors.black,
-                      )
-                    ],
-                  ),
-                  child: CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: NetworkImage(
-                      picURL,
+          child: ModalProgressHUD(
+            inAsyncCall: showSpinner,
+            child: Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/app-background-3.jpg'),
+                      fit: BoxFit.cover)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 7.0,
+                          color: Colors.black,
+                        )
+                      ],
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Text(
-                  StringUtils.capitalize(userName),
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '@Professor',
-                  style: TextStyle(
-                    color: Colors.teal[100],
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                SizedBox(
-                  height: 10.0,
-                ),
-
-                Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-                  child: ListTile(
-                    visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-                    leading: Icon(Icons.email, color: Colors.teal),
-                    title: Align(
-                      child: Text(
-                        emailId,
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontSize: 20.0,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      overflow: Overflow.visible,
+                      children: [
+                        _image!=null?
+                        CircleAvatar(
+                          radius: 50.0,
+                          backgroundColor: Colors.black12,
+                          child: ClipOval(
+                            child: SizedBox(
+                              width: 100.0,
+                              height: 100.0,
+                              child: (_image != null)
+                                  ? Image.file(
+                                _image,
+                                fit: BoxFit.fill,
+                              )
+                                  : Image.asset(
+                                'images/default-profile.jpg',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ):
+                        CircleAvatar(
+                            radius: 50.0,
+                            backgroundImage:NetworkImage(
+                                (picURL != null)
+                                ? picURL
+                                : 'https://firebasestorage.googleapis.com/v0/b/comperio-1071d.appspot.com/o/default-profile.webp?alt=media&token=52b10457-a10a-417e-b5af-3d84e5833fae'),
                         ),
-                      ),
-                      alignment: Alignment(-2.5, 0),
+                        Positioned(
+                          top: -1,
+                          right: -45,
+                          bottom: -50,
+                          left: 40,
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.camera,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            onPressed: () {
+                              getImages();
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    userName != null ? userName : 'abc',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '@Professor',
+                    style: TextStyle(
+                      color: Colors.teal[100],
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
 
-                // adding rating bar showing the overall average rating
+                  SizedBox(
+                    height: 10.0,
+                  ),
+
+                  Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+                    child: ListTile(
+                      visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                      leading: Icon(Icons.email, color: Colors.teal),
+                      title: Align(
+                        child: Text(
+                          emailId != null ? emailId : 'abc@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal[900],
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        alignment: Alignment.bottomLeft,
+                      ),
+                    ),
+                  ),
+
+                  // adding rating bar showing the overall average rating
 //              Container(
 //                RatingBar(
 //    initialRating: 3,
@@ -168,29 +209,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
 //      print(rating);
 //    },
 // );),
-                Container(
-                    height: 50.0,
-                    width: 170.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.blueAccent,
-                      color: Colors.blueAccent,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Send Message',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
+                  Container(
+                      height: 50.0,
+                      width: 170.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.blueAccent,
+                        color: Colors.blueAccent,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () async {
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            if (_image != null) {
+                              await uploadPic(context);
+                            }
+                            // print(url);
+                            FirebaseFirestore.instance.collection('users').doc(userName).set(
+                                {'profileURL': url,});
+                            setState(() {
+                              showSpinner = false;
+                            });
+                          },
+                          child: Center(
+                            child: Text(
+                              'Save',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ))
-              ],
+                      ))
+                ],
+              ),
             ),
           ),
         ),

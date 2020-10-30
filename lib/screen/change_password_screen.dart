19208cb2911f224792c42879ac1e418906d5ecc0
ritik <ritik.srivastava_cs18@gmail.com>
2020-10-30 22:30:41 +0530

    removed show spinner error from change password screen.

--- lib/screen/change_password_screen.dart
+++ lib/screen/change_password_screen.dart
@@ -4,7 +4,6 @@ import 'package:comperio/constants.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'login_screen.dart';
 
-
 class ChangePasswordScreen extends StatefulWidget {
   final String id = 'ChangePasswordScreen';
   @override
@@ -19,14 +18,13 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   String oldPassword;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
   //this method created to show dialogs
-  void _showDialog({String text, Function onPressed}){
+  void _showDialog({String text, Function onPressed}) {
     showDialog(
-      context:context,
-      builder: (BuildContext context){
+      context: context,
+      builder: (BuildContext context) {
         return AlertDialog(
-          title:Text(text),
+          title: Text(text),
           actions: <Widget>[
             FlatButton(
               child: Text('Ok'),
@@ -38,35 +36,50 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
     );
   }
 
-
   //change the password
-  void changePassword(String password) async{
-    var user =  await _auth.currentUser;
+  void changePassword(String password) async {
+    var user = await _auth.currentUser;
     validateUserPassword(oldPassword).then((_) {
-      user.updatePassword(password).then((_) {    //this method updating the password
-        _showDialog(text:'Password Changed Successfully!!!', onPressed : (){Navigator.pushNamed(context,LoginScreen().id);});
-      }).catchError((e) {     //catching error for updatedPassword
+      user.updatePassword(password).then((_) {
+        //this method updating the password
+        _showDialog(
+            text: 'Password Changed Successfully!!!',
+            onPressed: () {
+              Navigator.pushNamed(context, LoginScreen().id);
+            });
+      }).catchError((e) {
+        //catching error for updatedPassword
         print(e);
-        _showDialog(text: 'Password Not Changed!!!', onPressed: (){Navigator.of(context).pop();});
+        _showDialog(
+            text: 'Password Not Changed!!!',
+            onPressed: () {
+              Navigator.of(context).pop();
+            });
       });
-      }).catchError((onError){  //catching error for validateUserPassword
-        print(onError);
-        _showDialog(text: 'Wrong Password!!!', onPressed: (){Navigator.of(context).pop();});
-
+    }).catchError((onError) {
+      //catching error for validateUserPassword
+      print(onError);
+      _showDialog(
+          text: 'Wrong Password!!!',
+          onPressed: () {
+            Navigator.of(context).pop();
+          });
+      setState(() {
+        showSpinner = false;
       });
-
+    });
   }
+
   //checking the old password
-  validateUserPassword(String password) async{
+  validateUserPassword(String password) async {
     var firebaseUser = await _auth.currentUser;
 
     var authCredentials = EmailAuthProvider.credential(
         email: firebaseUser.email, password: password);
 
-    return firebaseUser
-        .reauthenticateWithCredential(authCredentials);
-
+    return firebaseUser.reauthenticateWithCredential(authCredentials);
   }
+
 //validating newPassword
   String validatePassword(String value) {
     Pattern pattern = r'^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$';
@@ -93,8 +106,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,10 +115,8 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
           padding: EdgeInsets.symmetric(horizontal: 30.0),
           constraints: BoxConstraints.expand(),
           decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                      "images/bg_p_image.jpg"),
-                  fit: BoxFit.cover),
+            image: DecorationImage(
+                image: AssetImage("images/bg_p_image.jpg"), fit: BoxFit.cover),
           ),
           child: Form(
             key: _formKey,
@@ -123,10 +132,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                   decoration: InputDecoration(
                     focusColor: Colors.white,
                     hintText: 'Enter Password',
-                    hintStyle: TextStyle(
-                        color: Colors.white12,
-                        fontSize: 18.0
-                    ),
+                    hintStyle: TextStyle(color: Colors.white12, fontSize: 18.0),
                     labelText: 'Old Password *',
                     labelStyle: TextStyle(color: Colors.white),
                     enabledBorder: UnderlineInputBorder(
@@ -136,12 +142,10 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       borderSide: BorderSide(color: Colors.white),
                     ),
                   ),
-
                   onChanged: (value) {
                     oldPassword = value;
                     //check password entered is of same user with which is it is loggedIn.
                   },
-
                 ),
                 SizedBox(
                   height: 20.0,
@@ -153,13 +157,10 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                   decoration: InputDecoration(
                     focusColor: Colors.white,
                     hintText: 'Enter Password',
-                    hintStyle: TextStyle(
-                      color: Colors.white12,
-                      fontSize: 18.0
-                    ),
+                    hintStyle: TextStyle(color: Colors.white12, fontSize: 18.0),
                     labelText: 'New Password *',
                     labelStyle: TextStyle(color: Colors.white),
-                     enabledBorder: UnderlineInputBorder(
+                    enabledBorder: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.white),
                     ),
                     focusedBorder: UnderlineInputBorder(
@@ -167,11 +168,10 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                     ),
                   ),
                   validator: validatePassword,
-                  onSaved: (value){
+                  onSaved: (value) {
                     newPassword = value;
                     //take the value in a variable so that it can be updated.
                   },
-
                 ),
                 SizedBox(
                   height: 30.0,
@@ -180,24 +180,28 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                     shape: StadiumBorder(),
                     padding: EdgeInsets.all(15.0),
                     color: Colors.lightBlueAccent,
-                   child: Text('Change Password' , style: KLoginRegistrationButtonStyle),
-                    onPressed:(){
+                    child: Text('Change Password',
+                        style: KLoginRegistrationButtonStyle),
+                    onPressed: () {
                       bool isShowSpinner = _validateInputs();
                       setState(() {
                         showSpinner = isShowSpinner;
                       });
 
-                      if(newPassword == oldPassword){   //condition if both the password are same
-                        _showDialog(text: 'Password is same as old password!!!',onPressed: (){Navigator.of(context).pop();});
+                      if (newPassword == oldPassword) {
+                        //condition if both the password are same
+                        _showDialog(
+                            text: 'Password is same as old password!!!',
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            });
                       }
                       changePassword(newPassword);
                       setState(() {
                         showSpinner = isShowSpinner;
                       });
                       print('password changed');
-
-                    }
-                    ),
+                    }),
               ],
             ),
           ),
